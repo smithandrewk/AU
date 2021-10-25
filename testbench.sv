a65d78f4f379528db09cbfd60c3abedcae80f5ce
Andrew <andrewsmith1025@gmail.com>
2021-10-24 22:52:02 -0400

    change clock freqs, delays, add tests

--- testbench.sv
+++ testbench.sv
@@ -19,17 +19,149 @@ module testbench ();
 	
 	//Simulates clock for testbench
 	always begin
-		clk = 0; #10; //May need to adjust delay for longer clock cycle
-		clk = 1; #10;
+		clk = 0; #1; //May need to adjust delay for longer clock cycle
+		clk = 1; #1;
 	end
 	
+
+	logic [31:0] temp;
+	logic temp_cout;
+	integer i , j, count;
 	//Initial block runs at the beginning of simulation
-	initial begin
-		assign ALUop = 2'b00;
-		assign a = 7;
-		assign b = 3;
+	initial 
 		//Apply the input values here, then check results
 		//Mult and div take 32 clock cycles each, so probably delay 33*clock cycle for safety?
 		//Check module 4 slide 41 for testbench structure
+	begin
+	// CHECK ADDITION
+	$display("CHECKING ADDITION");
+	a = 0;
+	b = 0;
+	ALUop = 2'b00;
+	count = 0;
+	for (i = 0; i <3; i= i+1)
+	begin
+		b = 32'h0;
+		for (j = 0; j < 3; j=j+1)
+		begin
+		#2;
+		b = b + 1;
+		temp = a + b;
+		#2;
+		//if (!(s == temp))
+		assert (s == temp)
+		$display("This is correct");
+		else begin
+			count = count + 1;
+			$display("Error number %d",count);
+			count = count + 1;
+		end
+		end
+		a = a + 1;
 	end
-endmodule
+	//CHECK SUB
+	$display("CHECKING SUBTRACTION");
+	a = 32'h0;
+	b = 32'h0;
+	ALUop = 2'b01;
+	count = 0;
+	for (i = 0; i <3; i= i+1)
+	begin
+		b = 32'h0;
+		a = i;
+		for (j = 0; j < 3; j=j+1)
+		begin
+		#2;
+		b = j;
+		{temp_cout,temp} = a - b;
+		#2;
+		//if (!(s == temp))
+		assert(s==temp)
+			$display("This is correct");
+		else begin
+			count = count + 1;
+			$display("Error number %d",count);
+			count = count + 1;
+		end   // else 
+		end
+	// a = a + 1;
+	end
+	#10;
+	//CHECK MULT
+	$display("CHECKING MULTIPLICATION");
+	a = 3;
+	b = 7;
+	ALUop = 2'b10;
+	rst_n = 0;
+	#3;
+	rst_n = 1;
+	temp = a*b;
+	#100
+	assert ({hi,lo} == temp)
+	$display("This is correct%d",{hi,lo});
+	else
+	$display("Nope");
+	a = 10;
+	b = 10;
+	ALUop = 2'b10;
+	rst_n = 0;
+	#3;
+	rst_n = 1;
+	temp = a*b;
+	#100
+	assert ({hi,lo} == temp)
+	$display("This is correct%d",{hi,lo});
+	else
+	$display("Nope");
+		a = 7;
+	b = 21;
+	ALUop = 2'b10;
+	rst_n = 0;
+	#3;
+	rst_n = 1;
+	temp = a*b;
+	#100
+	assert ({hi,lo} == temp)
+	$display("This is correct%d",{hi,lo});
+	else
+	$display("Nope");
+	//CHECK DIV
+	$display("CHECKING DIV");
+	a = 10;
+	b = 5;
+	ALUop = 2'b11;
+	rst_n = 0;
+	#3;
+	rst_n = 1;
+	temp = a/b;
+	#100
+	assert ({hi,lo} == temp)
+	$display("This is correct%d",{hi,lo});
+	else
+	$display("Nope");
+	a = 10;
+	b = 10;
+	ALUop = 2'b11;
+	rst_n = 0;
+	#3;
+	rst_n = 1;
+	temp = a/b;
+	#100
+	assert ({hi,lo} == temp)
+	$display("This is correct%d",{hi,lo});
+	else
+	$display("Nope");
+	a = 100;
+	b = 5;
+	ALUop = 2'b11;
+	rst_n = 0;
+	#3;
+	rst_n = 1;
+	temp = a/b;
+	#100
+	assert ({hi,lo} == temp)
+	$display("This is correct%d",{hi,lo});
+	else
+	$display("Nope");
+	end
+	endmodule
